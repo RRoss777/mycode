6cf4df039a81446558bfd3348f33fc3908af44dd
Ryanne Ross <RRoss777@users.noreply.github.com>
2022-10-14 17:24:39 +0000

    join and split

---
+++ challenge_shebang.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""TLG NDE Python | Challenge
+    print script containing user's name asking what day of the week it is"""
+    
+def main():
+    
+    #collect input asking the user's name
+    username = input("Please enter your username")
+    
+    #collect input asking what day of the week it is
+    day_of_the_week = input("What day of the week is it?")
+
+
+
+    #print one statement containing name and day of the week
+    print = ("Hello",username+"Happy",day_of_the_week)    
+
+    main()
