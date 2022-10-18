7bb69a65e8d7db7f0940020aa59c884ce3606491
Ryanne Ross <RRoss777@users.noreply.github.com>
2022-10-18 21:43:00 +0000

    Learning about conditional if

---
+++ iftest/condition01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Alta3 Research | RRoss
+Testing if Conditionals"""
+
+hostname = "MTG"
+
+if hostname == "MTG":
+    print("The hostname was found to be MTG")
---
+++ iftest/condition03.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Alta3 Research | RRoss
+Checking hostname against expected value"""
+
+##collect input from user
+
+hostname = input("What value should we set for hostname?")
+## Notice how the next line has changed
+## here we use the str.lower() method to return a lowercase string
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+
+#always display to user
+print("Exiting the script.")
+
